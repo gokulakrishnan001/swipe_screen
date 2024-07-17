46b605127e254a8332b1b04494d7e98d6e9b025a
gokulakrishnan001 <gokulakrishnan.g@outlook.com>
2024-07-17 18:37:04 +0530

    Create Swipe Screen

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<Widget> _list = <Widget>[
+    _HomeScreenState().ScatteredList(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,15 +33,19 @@ class _HomeScreenState extends State<HomeScreen> {
               Icon(MaterialIcons.arrow_back_ios),
               Text(
                 'Account',
-                style: TextStyle(color: Color.fromARGB(255, 2, 46, 82)),
+                style: TextStyle(color: Color.fromARGB(255, 41, 138, 218)),
               ),
             ],
           )),
       body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
         TextView(),
         ButtonView(),
-        SizedBox(
-          height: 428,
+        Expanded(
+          child: PageView.builder(
+              itemCount: 2,
+              reverse: true,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) => ScatteredList()),
         ),
         BottomView()
       ]),
@@ -48,7 +57,7 @@ class _HomeScreenState extends State<HomeScreen> {
       padding: EdgeInsets.all(20),
       child: const Text(
         "Who do you want to \ntransfer money to?",
-        style: TextStyle(fontSize: 35, color: Color.fromARGB(255, 61, 1, 71)),
+        style: TextStyle(fontSize: 30, color: Color.fromARGB(255, 61, 1, 71)),
       ),
     );
   }
@@ -80,8 +89,8 @@ class _HomeScreenState extends State<HomeScreen> {
     return Row(
       children: [
         Container(
-          width: 250,
-          height: 200,
+          width: 235,
+          height: 134,
           decoration: const BoxDecoration(
             color: Color.fromARGB(255, 38, 89, 197),
             borderRadius: BorderRadius.only(
@@ -106,19 +115,19 @@ class _HomeScreenState extends State<HomeScreen> {
                       fontWeight: FontWeight.bold),
                 ),
                 SizedBox(
-                  height: 10,
+                  height: 8,
                 ),
                 Text(
-                  "       drag to the person \n       you want to send money",
+                  "             drag to the person \n             you want to send money",
                   style: TextStyle(
-                      fontSize: 15,
+                      fontSize: 11,
                       color: Color.fromARGB(255, 207, 205, 205),
                       fontWeight: FontWeight.w100),
                 )
               ]),
         ),
         const SizedBox(
-          width: 30,
+          width: 18,
         ),
         const CircleAvatar(
           radius: 45,
@@ -128,6 +137,76 @@ class _HomeScreenState extends State<HomeScreen> {
             style: TextStyle(color: Colors.white),
           ),
         )
+      ],
+    );
+  }
+
+  Widget ScatteredList() {
+    return StaggeredGrid.count(
+      crossAxisCount: 4,
+      mainAxisSpacing: 4,
+      crossAxisSpacing: 4,
+      children: [
+        StaggeredGridTile.count(
+          crossAxisCellCount: 3,
+          mainAxisCellCount: 2,
+          child: CircleAvatar(
+            radius: 40,
+            backgroundColor: Colors.blueAccent,
+            child: Image.network(
+              'https://img.icons8.com/?size=100&id=23280&format=png&color=000000',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        StaggeredGridTile.count(
+          crossAxisCellCount: 1,
+          mainAxisCellCount: 2,
+          child: CircleAvatar(
+            radius: 40,
+            backgroundColor: Colors.amberAccent,
+            child: Image.network(
+              'https://img.icons8.com/?size=100&id=108337&format=png&color=000000',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        StaggeredGridTile.count(
+          crossAxisCellCount: 1,
+          mainAxisCellCount: 2,
+          child: CircleAvatar(
+            radius: 50,
+            backgroundColor: Colors.cyan,
+            child: Image.network(
+              'https://img.icons8.com/?size=100&id=18738&format=png&color=000000',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        StaggeredGridTile.count(
+          crossAxisCellCount: 3,
+          mainAxisCellCount: 2,
+          child: CircleAvatar(
+            radius: 40,
+            backgroundColor: Color.fromARGB(255, 201, 43, 140),
+            child: Image.network(
+              'https://img.icons8.com/?size=100&id=23301&format=png&color=000000',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        StaggeredGridTile.count(
+          crossAxisCellCount: 4,
+          mainAxisCellCount: 1,
+          child: CircleAvatar(
+            radius: 40,
+            backgroundColor: const Color.fromARGB(255, 236, 158, 129),
+            child: Image.network(
+              'https://img.icons8.com/?size=512w&id=23309&format=png',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
       ],
     );
   }
